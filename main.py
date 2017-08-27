37b7ec4ebcaae58129fa21a83b76412f4037bff7
Unknown <gauthier.menin@gmail.com>
2017-08-27 18:48:56 +0200

    Comments
    Comments

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-#init
-
+# init
+# this is a comment
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.nytimes.com'
-r = requests.get(url)
-r_html = r.text
+url = 'http://www.nytimes.com'  # defines what website to scrap
+r = requests.get(url) # creates a var "holding" the request
+r_html = r.text  # creates a var to which is written the result of the request
 soup = BeautifulSoup(r_html, "lxml")
 title = soup.findAll(class_="story-heading")
 
